f4f7f9e7586e3fe4d510eb29800aa7c2ce01631f
veds5513 <99479777+veds5513@users.noreply.github.com>
2025-04-29 05:06:40 +0530

    Update HPC2.cpp

--- HPC2.cpp
+++ HPC2.cpp
@@ -1,23 +1,6 @@
-#include<iostream>
-#include<stdlib.h>
-#include<omp.h>
+#include <iostream>
+#include <omp.h>
 using namespace std;
-
-void bubble(int *, int);
-void swap(int &, int &);
-
-void bubble(int *a, int n) {
-    for (int i = 0; i < n; i++) {
-        int first = i % 2;
-
-        #pragma omp parallel for shared(a, first)
-        for (int j = first; j < n - 1; j += 2) {
-            if (a[j] > a[j+1]) {
-                swap(a[j], a[j+1]);
-            }
-        }
-    }
-}
 
 void swap(int &a, int &b) {
     int temp = a;
@@ -25,24 +8,31 @@ void swap(int &a, int &b) {
     b = temp;
 }
 
-int main() {
-    int *a, n;
-    cout << "\nEnter total number of elements => ";
-    cin >> n;
-    a = new int[n];
-
-    cout << "\nEnter elements => ";
+void bubble(int *a, int n) {
     for (int i = 0; i < n; i++) {
-        cin >> a[i];
+        int first = i % 2;
+        #pragma omp parallel for
+        for (int j = first; j < n - 1; j += 2)
+            if (a[j] > a[j + 1])
+                swap(a[j], a[j + 1]);
     }
+}
+
+int main() {
+    int n;
+    cout << "\nEnter number of elements: ";
+    cin >> n;
+    int *a = new int[n];
+
+    cout << "\nEnter elements: ";
+    for (int i = 0; i < n; i++) cin >> a[i];
 
     bubble(a, n);
 
-    cout << "\nSorted array is => ";
-    for (int i = 0; i < n; i++) {
-        cout << a[i] << endl;
-    }
+    cout << "\nSorted array:\n";
+    for (int i = 0; i < n; i++) cout << a[i] << " ";
+    cout << endl;
 
-    delete[] a; // Optional: free memory
+    delete[] a;
     return 0;
 }
